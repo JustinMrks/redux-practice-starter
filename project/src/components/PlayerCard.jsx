0688d19376565bc28a4ca5db2b46f1e93b9f9ab0
justinmrks <justinmarks19@gmail.com>
2020-10-07 20:14:17 -0500

    Scaffolded components and rendered in correct spot

---
+++ project/src/components/PlayerCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlayerCard = () => {
+  return <div>Player Card</div>;
+};
+
+export default PlayerCard;
---
+++ project/src/components/PlayerList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlayerCard from "./PlayerCard";
+
+const PlayerList = () => {
+  return (
+    <div>
+      Player List
+      <PlayerCard />
+    </div>
+  );
+};
+
+export default PlayerList;
