eb7e9aa79ab7562e6e4d5d489f364f7e838e6176
justinmrks <justinmarks19@gmail.com>
2020-10-07 21:00:38 -0500

    completed player list with event handlers

--- project/src/components/PlayerList.jsx
+++ project/src/components/PlayerList.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PlayerCard from "./PlayerCard";
+import { connect } from "react-redux";
+import { fetchPlayers } from "../actions";
 
-const PlayerList = () => {
+const PlayerList = (props) => {
+  useEffect(() => {
+    props.fetchPlayers();
+  }, []);
+
   return (
     <div>
       Player List
-      <PlayerCard />
+      {props.players.map((player) => {
+        <PlayerCard player={player} key={player.id} />;
+      })}
     </div>
   );
 };
 
-export default PlayerList;
+const mapStateToProps = (state) => {
+  return {
+    players: state.players,
+    isLoading: state.isLoading,
+    errors: state.errors,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPlayers })(PlayerList);
